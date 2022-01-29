dbcc991aff865529de223bafb20f2dbece26707a
Everson Marqui Junior <studentprogramming48@gmail.com>
2022-01-29 10:02:35 -0300

    Mais Atualizações

---
+++ Extracurriculares/Obstáculos/script.js
@@ -0,0 +1,29 @@
+let personagem = document.querySelector('#personagem');
+let quadrado = document.querySelector('#quadrado');
+
+function pular(){
+    if(personagem.classList != 'animar'){
+        personagem.classList.add('animar')
+    }
+
+    setTimeout(function(){
+        personagem.classList.remove('animar')
+    }, 300)
+}
+
+var testarColisao = setInterval( function(){
+
+    var topoPersonagem = parseInt(
+        window.getComputedStyle(personagem).getPropertyValue('top')
+    )
+
+    var EsquerdaQuadrado = parseInt(
+        window.getComputedStyle(quadrado).getPropertyValue('left')
+    )
+
+        if(EsquerdaQuadrado < 20 && EsquerdaQuadrado > 0 && topoPersonagem >= 130){
+           quadrado.style.animation = 'none'
+           quadrado.style.display = 'none'
+           alert('Você Perdeu! Aperte F5 para tentar novamente') 
+        }   
+}, 10)
